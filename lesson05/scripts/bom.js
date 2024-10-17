c7e9c163c7d8f7cda57b0f3ba53e6a4f2b8e69ab
[byui-webweaver] <[1crayeak23@gmail.com]>
2024-10-16 21:47:13 -0600

    Lesson 05 added function

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,13 +1,17 @@
 const input = document.querySelector('#favchap');
+const notesInput = document.querySelector('#notes'); //Select the notes input
 const button = document.querySelector('button');
 const list = document.querySelector('#list')
 
 button.addEventListener('click', function() {
-    if (input.value != '') {
+    if (input.value != '' && notesInput.value != '') {
 
         const li = document.createElement('li');  //Create a new li item
         const deleteButton = document.createElement('button'); //Create a delete button
-        li.textContent = input.value; //Populate the li elements
+
+        //Populate the li elements with the chapter and notes
+        li.textContent = `${input.value} - Notes: ${notesInput.value}`;
+
         deleteButton.textContent = '✖️'; //Populate the button textContent with a x
         li.append(deleteButton); //Append the li element with the delete button
         list.append(li); //Append the li element to the ul in your HTML
@@ -20,8 +24,10 @@ button.addEventListener('click', function() {
 
         input.focus(); //Send the focus to the input element
         input.value = ''; //Change the input value to nothing
+        notesInput.value = ''; //Clear notes and input value
 
-
+    } else {
+        alert('Enter a chapter and an added note!');
     }
 });
     
