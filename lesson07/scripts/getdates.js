b207b0f4ca24eb787c224924532edf0f116a5081
[byui-webweaver] <[1crayeak23@gmail.com]>
2024-10-28 21:06:04 -0600

    Lazy Load Activity

---
+++ lesson07/scripts/getdates.js
@@ -0,0 +1,5 @@
+//set current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+//set modified date
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
