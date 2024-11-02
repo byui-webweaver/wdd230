0cb8ce218c19efb6604c76b1affc21efd2ceee1c
[byui-webweaver] <[1crayeak23@gmail.com]>
2024-11-01 22:02:18 -0600

    js fix

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {}
+document.addEventListener('DOMContentLoaded', function () {
 
     //Windchill calculations for weather 
     function calculateWindChill() {
