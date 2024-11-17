5ae2bbc9cb64bc58090edee6f441240e770bfbdf
[byui-webweaver] <[1crayeak23@gmail.com]>
2024-11-16 19:36:50 -0700

    changed out article

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,6 +1,6 @@
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+const display = document.querySelector(".cards-dir");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 display.classList.add("grid");
