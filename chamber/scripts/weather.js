3fe35bc5c424275b6535d8c1dc397e92b6042f1c
[byui-webweaver] <[1crayeak23@gmail.com]>
2024-11-21 15:20:50 -0700

    weather part chamber page

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,11 +1,14 @@
 //Select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
+const currentTemp = document.querySelector('#temperature');
+const conditions = document.querySelector('#cur-conditions');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-//Declare const variable assigned to a valid URL string from openweathermap apo documentation
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7499&lon=6.6374&units=imperial&appid=384a2edb7c93680cf99d1c73c465c0f3';
+const forecast = document.querySelector('#forecast');
 
-//Define asunchronous function named apiFetch with try block for errors
+//Declare const variable assigned to a valid URL string from openweathermap apo documentation
+const url = 'https://api.openweathermap.org/data/3.0/onecall?lat=40.457&lon=-111.776&units=imperial&appid=57a87d97169e547afbaf21e0b48b1a3b';
+
+//Define asynchronous function named apiFetch with try block for errors
 
 async function apiFetch() {
     try {
@@ -16,6 +19,7 @@ async function apiFetch() {
             displayResults(data); //call displayResults here
         } else {
             throw Error(await response.text());
+        }
     } catch (error) {
         console.log(error);
     }
@@ -26,19 +30,15 @@ apiFetch();
 
 function displayResults (data) {
     //Display rounded temperature
-    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
+    currentTemp.innerHTML = `${Math.round(data.current.temp)}&deg;F`;
 
     //icon sourse URL
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
 
     //Capitalize the description
-    const capitalizeDescription = (description => {
-        return description
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-    })
-    let desc = data.weather.map(item => capitalizeDescription(item.description)).join(', ');
+    const desc = data.current.weather[0].description.charAt(0).toUpperCase() + data.current.weather[0].description.slice(1);
+
+    conditions.innerHTML = desc;
 
     //Attributes for the icon
     weatherIcon.setAttribute('src', iconsrc);
@@ -46,5 +46,8 @@ function displayResults (data) {
 
     //Set caption text
     captionDesc.textContent = desc;
+
+    const threeDayForecast = data.daily.slice(1, 4).map(day => `${Math.round(day.temp.day)}℉`).join(', ');
+    forecast.innerHTML = threeDayForecast;
     
-} 
+}
